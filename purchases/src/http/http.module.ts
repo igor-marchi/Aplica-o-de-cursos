79a4e4750cc4c96bda50c1897858831db03a26dc
igor-marchi <igoraugustomarchi@gmail.com>
2022-06-13 21:51:07 -0300

    create kafka conection

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -5,6 +5,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import path from 'node:path';
 
 import { DatabaseModule } from '../database/database.module';
+import { MessagingModule } from '../messaging/messaging.module';
 import { CustomersService } from '../services/customers.service';
 import { ProductService } from '../services/products.service';
 import { PurchasesService } from '../services/purchases.service';
@@ -16,6 +17,7 @@ import { PurchasesResolver } from './graphql/resolvers/purcheases.resolver';
   imports: [
     ConfigModule.forRoot(),
     DatabaseModule,
+    MessagingModule,
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,4 +1,8 @@
-import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import {
+  getAccessToken,
+  useUser,
+  withPageAuthRequired,
+} from "@auth0/nextjs-auth0";
 
 export default function Home() {
   const { user } = useUser();
